ef41742b17a7fb52a816d9f602098c128f1dd1b0
Eddie Song <eddie.y.song@gmail.com>
2017-06-27 16:16:37 -0400

    player movement

---
+++ Assets/GameController.cs
@@ -0,0 +1,36 @@
+﻿// Eddie Song 2017-06-27
+// Game Controller Script, contains general configurations of the game
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField]
+	private Vector2 xRange, yRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	// g/setters
+	public Vector2 XRange {
+		get {
+			return this.xRange;
+		}
+	}
+
+	public Vector2 YRange {
+		get {
+			return this.yRange;
+		}
+	}
+}
---
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿// Eddie Song 2017-06-27
+// Player Ship Controller script, handles player movement/firing
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	// configurable properties of player ship
+	[SerializeField]
+	private float movespeed = 10f;
+
+	// configuration read from other scripts
+	private Vector2 xRange, yRange;
+
+	// handles to components
+	private Rigidbody2D rb2d;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		xRange = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ().XRange;
+		yRange = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ().YRange;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Movement according to input axis
+		rb2d.velocity = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * movespeed;
+		// Clamp position of player ship so it doesn't go outside boundaries
+		rb2d.position = new Vector2 (Mathf.Clamp (rb2d.position.x, xRange.x, xRange.y), Mathf.Clamp (rb2d.position.y, yRange.x, yRange.y));
+
+	}
+}
