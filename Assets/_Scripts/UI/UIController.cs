4f137a91c07ca1043a5406ee2aff910ff56278f1
Eddie Song <eddie.y.song@gmail.com>
2017-07-07 15:02:25 -0400

    Added a bunch of patterns

--- Assets/_Scripts/UI/UIController.cs
+++ Assets/_Scripts/UI/UIController.cs
@@ -8,7 +8,7 @@ public class UIController : MonoBehaviour {
 	private Text lives;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		lives = GameObject.Find ("Canvas/Lives Display").GetComponent<Text>();
 	}
 	
